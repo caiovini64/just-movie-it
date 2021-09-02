64ec946e8f08cc8d0166ca9166cd9a9119cb4526
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-09-02 09:45:12 -0300

    create PasswordInput component

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -2,3 +2,4 @@ export 'app_colors.dart';
 export 'app_theme.dart';
 export 'email_input.dart';
 export 'logo.dart';
+export 'password_input.dart';
---
+++ lib/ui/components/password_input.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:just_movie_it/ui/components/app_colors.dart';
+
+class PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: 'Password Input',
+      child: Container(
+        padding: const EdgeInsets.only(left: 10),
+        height: 60,
+        decoration: BoxDecoration(
+          color: accentColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: TextFormField(
+          keyboardType: TextInputType.visiblePassword,
+          style: Theme.of(context).textTheme.bodyText2,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            hintStyle: Theme.of(context).textTheme.bodyText2,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            suffixIcon: const Icon(Icons.visibility_off),
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            errorBorder: InputBorder.none,
+            focusedErrorBorder: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
