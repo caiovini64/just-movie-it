251245ab01403d197c9bc0d7b77a04b46a210524
Caio Jesus <caio.nascimento@grupofasitec.com.br>
2021-09-02 07:53:52 -0300

    add app theme

---
+++ lib/ui/components/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:just_movie_it/ui/components/app_colors.dart';
+
+class AppTheme {
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: backgroundColor,
+    buttonColor: primaryColor,
+    primaryColor: primaryColor,
+    accentColor: accentColor,
+  );
+}
