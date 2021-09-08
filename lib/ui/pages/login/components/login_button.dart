bb59de305ea20bb0939e6505bacc726ed99eae41
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-09-07 21:43:28 -0300

    add loading in LoginButton when state is Loading

--- lib/ui/pages/login/components/login_button.dart
+++ lib/ui/pages/login/components/login_button.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_movie_it/ui/components/components.dart';
+import 'package:just_movie_it/ui/pages/login/presentation/cubit_login_presenter.dart';
 
 class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StandardButton(
-      onPressed: () {},
-      child: const Text('Login'),
+    final presenter = context.read<CubitLoginPresenter>();
+    return BlocBuilder<CubitLoginPresenter, LoginState>(
+      builder: (context, state) {
+        return StandardButton(
+          onPressed: presenter.login,
+          child: state is Loading
+              ? const CircularProgressIndicator(color: Colors.white)
+              : const Text('Login'),
+        );
+      },
     );
   }
 }
