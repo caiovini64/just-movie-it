1e43e7e3f035937b34a0b25a230126fd5f8593c8
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-09-08 21:14:37 -0300

    create abstract AuthDatasource

--- lib/domain/datasources/auth_datasource.dart
+++ lib/domain/datasources/auth_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:just_movie_it/domain/entities/entities.dart';
 import 'package:just_movie_it/domain/helpers/parameters/parameters.dart';
 
-abstract class AuthRepository {
+abstract class AuthDatasource {
   Future<UserEntity> login(AuthParameters authParameters);
 }
