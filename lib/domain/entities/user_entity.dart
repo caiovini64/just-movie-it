4e7266505a17eb44a1d99435b3e816a390130546
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-09-08 21:02:23 -0300

    create UserEntity

---
+++ lib/domain/entities/user_entity.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String id;
+  final String email;
+  final String token;
+
+  const UserEntity(this.id, this.email, this.token);
+
+  @override
+  List<Object?> get props => [id, email, token];
+}
