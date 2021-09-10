f9948ac912525d8414d4d57c79122a81e8e3e578
caio <caioj3505@gmail.com>
2021-09-10 14:39:20 -0300

    add AuthRepository in CubitLoginPresenter

--- lib/ui/pages/login/presentation/cubit_login_presenter.dart
+++ lib/ui/pages/login/presentation/cubit_login_presenter.dart
@@ -1,13 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:just_movie_it/domain/repositories/repositories.dart';
 import 'package:just_movie_it/ui/pages/login/login_presenter.dart';
 import 'package:meta/meta.dart';
 
 part 'cubit_login_state.dart';
 
 class CubitLoginPresenter extends Cubit<LoginState> implements LoginPresenter {
-  CubitLoginPresenter() : super(Initial());
+  final IAuthRepository repository;
+  CubitLoginPresenter(this.repository) : super(Initial());
 
   @override
   void goToSignUp() {
